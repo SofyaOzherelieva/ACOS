7bac4185fa0a9bccc3d357ce6dba6183e6375674
SofyaOzherelieva <sofya.ozherelieva@yandex.ru>
2017-04-17 20:40:06 +0300

    Update myCP:3.c
    handle errors, fix copyFILE_TO_DIR

--- lab1/CP/myCP:3.c
+++ lab1/CP/myCP:3.c
@@ -6,13 +6,67 @@
 #include <sys/stat.h>
 
 
-static int copyFILE_TO_FILE(char* source, char* destination);
-static int copyFILE_TO_DIR(char* source, char* destination);
+enum operation {FILE_TO_FILE, FILE_TO_DIR};
 
-enum operation { FILE_TO_FILE, FILE_TO_DIR};
+static int copyFILE_TO_FILE(char* source, char* destination) {
+    FILE* copyFrom = fopen(source, "r");
 
-int main( int argc, char *argv[] )
-{
+    if (copyFrom == NULL) {
+        fprintf(stderr, "Open file failed\n");
+        return 1;
+    }
+    
+    FILE* copyTo = fopen(destination, "w");
+
+    // If there is no such file, we will create it.
+    if (copyTo == NULL) {
+        copyTo = fopen(destination, "wb");
+        if (copyTo == NULL) {
+            fprintf(stderr, "Create file failed\n");
+            return 2;
+        } 
+    }
+    int caractereActuel = fgetc(copyFrom);
+    for (; caractereActuel != EOF; caractereActuel = fgetc(copyFrom)) {
+        fputc(caractereActuel, copyTo);
+    }
+    fclose(copyFrom);
+    fclose(copyTo);
+    return 0;
+}
+
+static int copyFILE_TO_DIR(char* source, char* destination) {
+    FILE* copyFrom = fopen(source, "r" );
+
+    if (copyFrom == NULL) {
+        fprintf(stderr, "Open file failed\n");
+        return 1;
+    }
+
+    char newFileName[256] = {0};
+    sprintf(newFileName, "%s%s%s", destination, "/",source);
+
+    FILE* copyTo = fopen(newFileName, "w");
+
+    // If there is no such file, we will create it.
+    if (copyTo == NULL) {
+        copyTo = fopen(newFileName, "wb");
+        if (copyTo == NULL) {
+            fprintf(stderr, "Create file failed\n");
+            return 2;
+        } 
+    }
+    int caractereActuel = fgetc(copyFrom);
+    for (; caractereActuel != EOF; caractereActuel = fgetc(copyFrom)) {
+        fputc(caractereActuel, copyTo);
+    }
+    fclose(copyFrom);
+    fclose(copyTo);
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
     struct stat source_stat, destination_stat;
     enum operation type;
 
@@ -20,51 +74,16 @@ int main( int argc, char *argv[] )
     argv[argc] = NULL;
     char* source = argv[--argc];
     argv[argc] = NULL;
-    int r = stat( destination, &destination_stat );
-    if( r == -1 || !S_ISDIR( destination_stat.st_mode ) ) {
+    int r = stat(destination, &destination_stat);
+    if (r == -1 || !S_ISDIR(destination_stat.st_mode)) {
         type = FILE_TO_FILE;
     } else
         type = FILE_TO_DIR;
 
-    if( type == FILE_TO_FILE ) {
-        exit( copyFILE_TO_FILE( source, destination ) );
+    if (type == FILE_TO_FILE){
+        exit (copyFILE_TO_FILE(source, destination));
     }
-    if( type == FILE_TO_DIR ) {
-        exit( copyFILE_TO_DIR( source, destination ) );
+    if (type == FILE_TO_DIR){
+        exit (copyFILE_TO_DIR(source, destination));
     }
-}
-
-static int copyFILE_TO_FILE( char* source, char* destination )
-{
-    FILE* copyFrom = fopen( source, "r" );
-    FILE* copyTo = fopen( destination, "w" );
-    for( ;;) {
-        int caractereActuel = fgetc( copyFrom );
-        if( caractereActuel != EOF ) {
-            fputc( caractereActuel, copyTo );
-        } else {
-            break;
-        }
-    }
-    fclose( copyFrom );
-    fclose( copyTo );
-    return 0;
-}
-static int copyFILE_TO_DIR( char* source, char* destination )
-{
-    FILE* copyFrom = fopen( source, "r" );
-    char newFileName[255] = { 0 };
-    sprintf( newFileName, "%s/%s", *destination, copyFrom );
-    FILE* copyTo = fopen( newFileName, "w" );
-    for( ;;) {
-        int caractereActuel = fgetc( copyFrom );
-        if( caractereActuel != EOF ) {
-            fputc( caractereActuel, copyTo );
-        } else {
-            break;
-        }
-    }
-    fclose( copyFrom );
-    fclose( copyTo );
-    return 0;
 }
